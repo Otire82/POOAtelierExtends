0daee14393cfc2b71b99775afeb80848e4f9f209
Otire82 <a.jeanne01@sncf.fr>
2022-11-08 15:22:26 +0100

    finish

--- AirMonster.java
+++ AirMonster.java
@@ -1,7 +1,20 @@
-class AirMonster {
+class AirMonster extends Monster {
 
     // TODO : add constructor and initialize type to "air"
+    public AirMonster(String name, int attack) {
+        super(name, attack, "air");
+    }
     
+    @Override
     // TODO : create an attack method which deals x2 damage to "water"
-    
+    public boolean attack(Monster opponent) {
+
+        int damage = this.getAttack();
+        if (opponent.getType().equals("water")) {
+            damage *=2;
+        }
+        opponent.setLife(opponent.getLife() - damage);
+
+        return opponent.isKO();
+    }
 }
